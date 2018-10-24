04fb003d345350842a3090e6dfe7cba15ef18fa3
Quy Nguyen <nguyenqb24@gmail.com>
2018-10-24 12:48:00 -0400

    starting express

---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,14 @@
+
+var matchProfile= require("../data/friends.js")
+
+module.export = function(app){
+app.get("/api/friends", function(req, res) {
+    res.json(matchProfile);
+  });
+
+// app.post
+app.post("/api/friends", function(req, res) {
+    
+  });
+}
+ 
---
+++ app/routing/htmlRoutes.js
@@ -0,0 +1,16 @@
+// ===============================================================================
+// DEPENDENCIES
+// We need to include the path package to get the correct file path for our html
+// ===============================================================================
+var path = require("path");
+
+module.exports = function(app) {
+
+app.get("*", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/home.html"));
+  });
+  
+  app.get("/survey", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/survey.html"));
+  });
+}
